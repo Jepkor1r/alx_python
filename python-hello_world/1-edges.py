e03353857ed6388162cffa132b5e5d7508aff3b1
Jepkor1r <jlagat007@gmail.com>
2023-07-18 22:56:45 +0300

    updated

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton" "School"
-word_first_3 = "First 3 letter"
-word_last_2 = "Last 2 letters"
-middle_word = "Middle word"
-print("First 3 letters: {}".format("Hol "  + "Sch"))
-print("Last 2 letters: {}".format("on " + "ol"))
-print("Middle word: {}".format("olberto " + "choo"))
+print("First 3 letters: {}".format("Hol"))
+print("Last 2 letters: {}".format("on"))
+print("Middle word: {}".format("olberto"))
+print("First 3 letters: {}".format("Sch"))
+print("Last 2 letters: {}".format("ol"))
+print("Middle word: {}".format("choo"))
