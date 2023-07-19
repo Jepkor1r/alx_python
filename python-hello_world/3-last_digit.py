33f84032fd28012047d1c0fb1d99fdd314fe27a1
Jepkor1r <jlagat007@gmail.com>
2023-07-19 20:43:39 +0300

    updated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,7 +9,5 @@ if int(last_digit) > 0:
  print("and is greater than 5")
 elif int(last_digit) == 0:
  print("and is 0")
-elif int(last_digit) < 6 and int(last_digit) != 0:
- print("and is less than 6 and not 0")
 else:
- print("wrong output")
+ print("and is less than 6 and not 0")
