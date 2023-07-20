608455f6c50c9e25fa93d459ab9e3d53d3c7050e
Jepkor1r <jlagat007@gmail.com>
2023-07-20 14:41:50 +0300

    updated

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j), end="")
+        if i == 8 and j == 9:  # Check if it's the last combination
+            print("{:02d}".format(i * 10 + j))
+        else:
+            print("{:02d}, ".format(i * 10 + j), end="")
