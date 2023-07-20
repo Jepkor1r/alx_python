c3983b3f4824bbcf1e5320b35902d6476a6af183
Jepkor1r <jlagat007@gmail.com>
2023-07-20 23:08:01 +0300

    updated

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -4,7 +4,12 @@ def fibonacci_sequence(n):
     if n <= 0:
         return []
 
-    fib_sequence = [0, 1]
+    fib_sequence = [0]
+
+    if n == 1:
+        return fib_sequence
+
+    fib_sequence.append(1)
 
     while len(fib_sequence) < n:
         next_fib = fib_sequence[-1] + fib_sequence[-2]
