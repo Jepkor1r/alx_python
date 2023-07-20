5fb739f39f3987e6143416c7035fc2994c431484
Jepkor1r <jlagat007@gmail.com>
2023-07-20 22:22:34 +0300

    Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fib_sequence = [0, 1]
+
+    while len(fib_sequence) < n:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+
+    return fib_sequence
+
+(fibonacci_sequence(5))
+(fibonacci_sequence(0))   
+(fibonacci_sequence(1))   
+(fibonacci_sequence(10)) 
