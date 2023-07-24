0efd65568f24bb5030da8990022d7a3d345f4060
Jepkor1r <jlagat007@gmail.com>
2023-07-24 22:07:52 +0300

    updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 from add_0 import add
-a=1
-b=2
 
-result= add(a, b)
+def main():
+    global a, b
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
 
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    main()
