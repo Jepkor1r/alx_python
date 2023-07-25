31570207155a308ac8d514aa331c4cd1edf873cf
Jepkor1r <jlagat007@gmail.com>
2023-07-25 12:18:25 +0300

    updated

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -7,18 +7,23 @@ def main():
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
 
-    print(f"Number of argument(s): {num_arguments}", end="")
+    output_lines = []
+    output_lines.append(f"Number of argument(s): {num_arguments}")
 
     if num_arguments == 0:
-        print(".")
+        output_lines.append(".")
     elif num_arguments == 1:
-        print(f", argument: {arguments[0]}")
+        output_lines.append(", argument:")
+        output_lines.append(f"1: {arguments[0]}")
     else:
-        print(", arguments:")
-
-        # Print each argument and its position
+        output_lines.append(", arguments:")
         for i, arg in enumerate(arguments, start=1):
-            print(f"{i}: {arg}")
+            output_lines.append(f"{i}: {arg}")
+
+    output = "\n".join(output_lines)
+    print(output)
+    print(f"\n({len(output)} chars long)")
 
 if __name__ == "__main__":
     main()
+    
