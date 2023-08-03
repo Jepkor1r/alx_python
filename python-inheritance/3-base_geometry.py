62fdfdda01617138d406767d5bf2da5c5fff01d3
Jepkor1r <jlagat007@gmail.com>
2023-08-03 23:53:39 +0300

    updated

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,6 +8,19 @@
     """
 class BaseGeometry:
     """
+    A base class representing geometry.
+    """
+
+
+class BaseGeometryMetaClass(type):
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """
     A base class representing geometry
     """
+    def __dir__(self):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     pass
+
