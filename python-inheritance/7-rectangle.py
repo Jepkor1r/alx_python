78d09ddd93833c05c0a94b718cf0575d415734e4
Jepkor1r <jlagat007@gmail.com>
2023-08-04 08:30:09 +0300

    updated

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -26,7 +26,20 @@ class BaseGeometry:
     This class is intended to be used as a base for other geometry-related classes.
     It currently does not have any attributes or methods defined.
     """
+    
+class BaseGeometryMetaClass(type):
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """
+    A base class representing geometry
+    """  
+    def __dir__(self):
+        """
+        Customization of the attributes visible when calling `dir()`.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     def area(self):
         """
         Calculate the area of the geometry.
