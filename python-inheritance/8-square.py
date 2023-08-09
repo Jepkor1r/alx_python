b3672e744b53b2ef8606916969e7e637d74643dc
Jepkor1r <jlagat007@gmail.com>
2023-08-09 21:56:14 +0300

    updated

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,24 +1,8 @@
 #!usr/bin/python3
 
 """
-    A base class representing geometry.
-
-    It is to be used as a base for other geometry-related classes.
-    It currently does not have any attributes or methods defined.
-
-    Public Methods:
-    - area(self): Calculate the area of the geometry.
-        Raises:
-         Exception: This method is not implemented in the base class.
-
-    - integer_validator(self, name, value): Validate an integer value.
-        Parameters:
-         name (str): The name of the value being validated.
-         value: The value to be validated.
-        Raises:
-         TypeError: If the value is not an integer.
-         ValueError: If the value is less than or equal to 0.
-    """
+    This module that defines the Rectangle and Square classes.
+"""
 
 
 class BaseGeometryMetaClass(type):
@@ -35,7 +19,7 @@ class BaseGeometryMetaClass(type):
             if attribute != '__init_subclass__'
             ]
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__("7-rectangle").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
