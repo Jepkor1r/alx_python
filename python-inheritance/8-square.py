a771121b285217eeb1432f14af5f703f759874ff
Jepkor1r <jlagat007@gmail.com>
2023-08-09 21:19:26 +0300

    updated

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -30,6 +30,53 @@ class BaseGeometryMetaClass(type):
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle, inheriting from BaseGeometry.
+
+    Public Methods:
+    - __init__(self, width, height):
+      Initialize a rectangle with width and height.
+    - area(self):
+      Calculate the area of the rectangle.
+    - __str__(self):
+      Return a string representation of the rectangle.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a rectangle with width and height.
+
+        Parameters:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        Calculate the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return a string representation of the rectangle.
+
+        Returns:
+         str: A string with the rectangle description.
+         In the format '[Rectangle] <width>/<height>'.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
 class Square(Rectangle):
     """
     A class representing a square, inheriting from Rectangle.
