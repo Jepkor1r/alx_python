e2002ed3ccf54ddef9c97504ce72613215f93488
Jepkor1r <jlagat007@gmail.com>
2023-08-10 20:08:04 +0300

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ from models.base import Base
 class Rectangle:
     """Rectangle class which inherits from Base class"""
     def __init__(self, width, height, x=0, y=0, id=None,):
-        """Initialize a rectangle.
+        """Initialize a rectangle
 
         Args:
             width: Rectangle width
