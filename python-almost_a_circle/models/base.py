3e532efc6a86350c6ebaedd40175f4daa2643984
Jepkor1r <jlagat007@gmail.com>
2023-08-10 15:04:41 +0300

    Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,17 +8,15 @@ class Base:
      """Base class for managing id attributes"""
      __nb_objects = 0
 
-def __init__(self, id=None):
-     """
+     def __init__(self, id=None):
+       """
         Initialize the Base instance.
 
         Args:
-            id (int, optional): The ID value for the instance. Defaults to None.
+          id (int, optional): The ID value for the instance. Defaults to None.
         """
-     if id is not None:
-        self.id = id
-     else:
-        Base.__nb_objects +=1
-        self.id = Base.__nb_objects
-        
-instance1 = Base()
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
