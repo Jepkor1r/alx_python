ba3145890d4dd2420da4570e3f45c35ed1f3c87b
Jepkor1r <jlagat007@gmail.com>
2023-08-10 20:45:06 +0300

    updated

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ from models.base import Base
 
 class Rectangle:
     """Rectangle class which inherits from Base class"""
-    def __init__(self, width, height, x=0, y=0, id=None,):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a rectangle.
 
         Args:
@@ -24,6 +24,12 @@ class Rectangle:
         self.__x = x
         self.__y = y
 
+        self._validate_width()
+        self._validate_height()
+        self._validate_x()
+        self._validate_y()
+
+
     @property
     def width(self):
         """Getting the Rectangle width"""
@@ -32,6 +38,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setting the Rectangle width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -42,6 +52,10 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setting the Rectangle height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -52,6 +66,10 @@ class Rectangle:
     @x.setter
     def x(self, value):
         """Setting the Rectangle x_value"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -62,4 +80,36 @@ class Rectangle:
     @y.setter
     def y(self, value):
         """Setting the Rectangle y_value"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0") 
         self.__y = value
+    
+    def _validate_width(self):
+        """Validating the width attribute"""
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
+
+    def _validate_height(self):
+        """Validating the height attribute"""
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
+
+    def _validate_x(self):
+        """Validating the x attribute"""
+        if not isinstance(self.__x, int):
+            raise TypeError("x must be an integer")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
+
+    def _validate_y(self):
+        """Validating the y attribute"""
+        if not isinstance(self.__y, int):
+            raise TypeError("y must be an integer")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
