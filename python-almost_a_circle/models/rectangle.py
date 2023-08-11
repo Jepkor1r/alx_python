8c1c0e8b51202cc57bb9fa71c22e014737cad8f0
Jepkor1r <jlagat007@gmail.com>
2023-08-11 15:23:42 +0300

    __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -133,3 +133,12 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """
+        Overrides __str__
+
+        Returns:
+            formatted string of Rectange attributes.
+        """
+        return("[Rectangle] ({}) {}/{} - {}/{}").format(self.id, self.__x, self.__y, self.__width, self.__height)
