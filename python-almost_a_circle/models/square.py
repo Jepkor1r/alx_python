17a988b1d32f16c3621553f745d1ba3f397fcc6f
Jepkor1r <jlagat007@gmail.com>
2023-08-11 16:11:11 +0300

    And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,13 +9,13 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """class Square which inherits from class Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a rectangle.
+        """Initialize a Square.
 
            Args:
-            width: Rectangle width
-            height: Rectangle height
-            x: Rectangle x_value
-            y: Rectangle y_value
+            width: Square width
+            height: Square height
+            x: Square x_value
+            y: Square y_value
         """
         super().__init__(id, x, y, size, size)
     def __str__(self):
