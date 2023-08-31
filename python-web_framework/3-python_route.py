8e8f2e7139b770fac54d0d534be562636a733db1
Jepkor1r <jlagat007@gmail.com>
2023-08-31 19:58:48 +0300

    Python is cool!

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -42,16 +42,15 @@ def c(text, strict_slashes=False):
     return f"C {escape(text)}"
 
 # Define a route for the "python/<text>" URL
+@app.route("/python/", defaults={"text":"is cool"})
 @app.route("/python/<text>")
-def python(text=None, strict_slashes=False):
+def python(text, strict_slashes=False):
     """"
       This function handles requests to the "python/<text>" URL of the web application.
     
     Returns:
         str: A message "Python followed by the value of the text variable" 
     """
-    if text is None:
-        text="is cool"
     text = text.replace("_", " ")
     return f"Python {escape(text)}"
 
