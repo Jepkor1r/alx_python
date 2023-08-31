a9f850a13935ae64617c43e5726b2f7204b2c5c7
Jepkor1r <jlagat007@gmail.com>
2023-08-31 15:57:12 +0300

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -5,10 +5,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/, strict_slashes=False")
-def index():
+@app.route("/")
+def index(strict_slashes=False):
     return "Hello HBNB!"
 
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port=5000)
- 
