50047e1a2ea0a12a49cca2e6642f082385093216
Jepkor1r <jlagat007@gmail.com>
2023-10-08 14:36:32 +0300

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,13 +2,17 @@
 Python script that retrieves employees todos list
 
 """
-
 import requests
 import sys
 
-def employees_todo_list(ID):
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/1/todos')
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/1')
+def employees_todo_list(employee_id):
+    # Construct the API endpoints based on the provided employee_id
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+
+    # Make API requests
+    todos_response = requests.get(todos_url)
+    user_response = requests.get(user_url)
     
     if todos_response.status_code == 200 and user_response.status_code == 200:
         todos_data = todos_response.json()
@@ -16,20 +20,21 @@ def employees_todo_list(ID):
         # Calculate the number of completed and total tasks
         total_tasks = len(todos_data)
         completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-        # output of the employee's todo list progress
+        # Output the employee's todo list progress
         employee_progresslist = 'Employee {} is done with tasks ({}/{}):'.format(user_data['name'], completed_tasks, total_tasks)
-        # titles of completed tasks
-        task_tittle =['\t{}'.format(todo['title']) for todo in todos_data if todo['completed']]
-        print(employee_progresslist +'\n'.join(task_tittle))      
+        # Titles of completed tasks
+        task_titles = ['\t{}'.format(todo['title']) for todo in todos_data if todo['completed']]
+        progress = '\n'.join([employee_progresslist] + task_titles)
+        print(progress)
     else:
-        print('HTTPS request failed with status code: {}'.format(todos_response.status_code))
+        print('HTTPS request failed with status codes: todos={}, user={}'.format(todos_response.status_code, user_response.status_code))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <ID>")
+        print("Usage: python script.py <employee_id>")
     else:
         try:
-            ID = int(sys.argv[1])
-            employees_todo_list(ID)
+            employee_id = int(sys.argv[1])
+            employees_todo_list(employee_id)
         except ValueError:
             print("Please enter a valid integer for the employee ID.")
