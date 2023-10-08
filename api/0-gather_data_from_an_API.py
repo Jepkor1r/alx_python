ecfcd6917ea302446916b2e1180f56853b68c189
Jepkor1r <jlagat007@gmail.com>
2023-10-08 11:52:52 +0300

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,21 +1,24 @@
+"""
+Python script that retrieves employees todos list
+
+"""
+
 import requests
 
-todos_response = requests.get('https://jsonplaceholder.typicode.com/users/1/todos')
-user_response = requests.get('https://jsonplaceholder.typicode.com/users/1')
-
-if todos_response.status_code == 200 and user_response == 200:
-    todos_data = todos_response.json()
-    user_data = user_response.json()
- # Calculate the number of completed and total tasks
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-    # Print the employee's progress
-    print('Employee {} is done with tasks ({}/{}):\n'.format(user_data['name'], completed_tasks, total_tasks))
-
-    # Print the titles of completed tasks
-    for todo in todos_data:
-        if todo['completed']:
-            print('\t{}'.format(todo['title']))
-else:
-    print('HTTPS request failed with status code: {}'.format(todos_response.status_code))
+def employees_todo_list(ID):
+    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/1/todos')
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/1')
+    
+    if todos_response.status_code == 200 and user_response.status_code == 200:
+        todos_data = todos_response.json()
+        user_data = user_response.json()
+        # Calculate the number of completed and total tasks
+        total_tasks = len(todos_data)
+        completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+        # output of the employee's todo list progress
+        employee_progresslist = 'Employee {} is done with tasks ({}/{}):'.format(user_data['name'], completed_tasks, total_tasks)
+        # titles of completed tasks
+        task_tittle =['\t{}'.format(todo['title']) for todo in todos_data if todo['completed']]
+        return employee_progresslist +'\n'.join(task_tittle)      
+    else:
+        return 'HTTPS request failed with status code: {}'.format(todos_response.status_code)
