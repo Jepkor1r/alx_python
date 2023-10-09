f728ceff196d445cc656376d3c0a9930c8881a34
Jepkor1r <jlagat007@gmail.com>
2023-10-09 21:13:17 +0300

    Export CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,3 +1,4 @@
+""" 
 import csv
 import os
 import requests
@@ -45,3 +46,45 @@ if __name__ == "__main__":
             employees_todo_list(employee_id)
         except ValueError:
             print("Please enter a valid integer for the employee ID.")
+"""
+
+"""
+Python script that retrieves employees todos list
+Employees data to be exported in the CSV format
+"""
+import csv
+import requests
+import sys
+
+
+def employees_todo_list(employee_id):
+    # Construct the API endpoints based on the provided employee_id
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+
+    # Make API requests
+    todos_response = requests.get(todos_url)
+    user_response = requests.get(user_url)
+
+    todos_data = todos_response.json()
+    user_data = user_response.json()
+
+    alltask_record  = []
+
+    for todo_data in todos_data:
+        alltask_record.append(
+            [
+                employee_id,
+                user_data,
+                todo_data["completed"],
+                todo_data["title"],
+            ]
+        )
+
+    with open(str(employee_id) + ".csv", "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(alltask_record)
+
+
+if __name__ == "__main__":
+    employees_todo_list(sys.argv[1])
