663237fe440c31dfa487ce2d20facf555a34c28b
Jepkor1r <jlagat007@gmail.com>
2023-10-11 06:58:47 +0300

    export to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,28 +7,30 @@ import csv
 import requests
 import sys
 
+def export_to_CSV(user_id):
+    employee_name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}".format(user_id)
+    ).json()[0]["username"]
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    ).json()
 
-def employees_todo_list(employee_id):
-  
-    todos_data = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)).json()
-    employee_name = requests.get("https://jsonplaceholder.typicode.com/users?={}".format(employee_id)).json()[0]["username"]
+    tasks_data = []
 
-    alltask_record  = []
-
-    for todo_data in todos_data:
-        alltask_record.append(
+    for task in tasks:
+        tasks_data.append(
             [
-                employee_id,
+                user_id,
                 employee_name,
-                todo_data["completed"],
-                todo_data["title"],
+                task["completed"],
+                task["title"],
             ]
         )
 
-    with open(str(employee_id) + ".csv", "w", encoding="UTF8", newline="") as f:
+    with open(str(user_id) + ".csv", "w", encoding="UTF8", newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        writer.writerows(alltask_record)
+        writer.writerows(tasks_data)
 
 
 if __name__ == "__main__":
-    employees_todo_list(sys.argv[1])
+    export_to_CSV(sys.argv[1])
