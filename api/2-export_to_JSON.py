ae8add989e1437926d8f2d78b6d9918432cd7dbb
Jepkor1r <jlagat007@gmail.com>
2023-10-12 06:22:54 +0300

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,37 +4,46 @@ Python script to export data in Json format
 
 import json
 import requests
+import sys
 
-def get_all_employees_info():
-    # Get all employees
-    employees_url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(employees_url)
-    employees = response.json()
+def employees_todo_list(employee_id):
+   # Construct the API endpoints based on the provided employee_id
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
 
-    # Prepare data for JSON
-    all_employee_data = {}
-    for employee in employees:
-        user_id = employee['id']
-        username = employee['username']
+    # Make API requests
+    todos_response = requests.get(todos_url)
+    user_response = requests.get(user_url)
 
-        # Get employee's TODO list
-        todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-        response = requests.get(todos_url)
-        todos = response.json()
+    todos_data = todos_response.json()
+    user_data = user_response.json()
 
-        # Prepare TODO data for the current employee
-        employee_todos = []
-        for todo in todos:
-            task_data = {
-                "username": username,
-                "task": todo['title'],
-                "completed": todo['completed']
+
+    employee_name = user_data.get("username")
+
+    # Create a list to store the tasks
+    tasks = []
+
+    # Create a dictionary to store the data
+    for todo_data in todos_data:
+        task = {
+            "task": todo_data["title"],
+            "completed": todo_data["completed"],
+            "username": employee_name
             }
-            employee_todos.append(task_data)
+        tasks.append(task)
+        
+    # Create a dictionary with the employee_id as the key and the list of tasks as the value
+    employee_data = {employee_id: tasks}
 
-        # Add TODO data to the dictionary
-        all_employee_data[user_id] = employee_todos
+    # Convert the dictionary to a JSON string
+    json_output = json.dumps(employee_data, indent=4)
 
-    # Write data to JSON file
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(all_employee_data, jsonfile, indent=4)
+      # Save the JSON data to a local file
+    filename = f"{employee_id}.json" 
+    with open(filename, "w") as json_file:
+        json.dump(employee_data, json_file, indent=4)
+
+if __name__ == "__main__":
+   employee_id = int(sys.argv[1])
+   employees_todo_list(employee_id)
